8e9b36f0e9c72d1c9c19dba97cb7a56cd36759d2
vinicius.oliveira <vinicius.oliveira@sequor.com.br>
2021-05-30 20:55:16 -0300

    api com jwt - https://youtu.be/UZ2Qa4xosl0

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shop.Models;
+using Shop.Repositories;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [ApiController]
+    [Route("account")]
+    public class HomeController : ControllerBase
+    {
+     
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+           try
+            {
+                var user = UserRepository.Get(model.Username, model.Password);
+
+                if (user == null)
+                    return NotFound(new { message = "Usuário ou senha inválidos" });
+
+                var token = TokenService.GenerateToken(user);
+                user.Password = "";
+
+                return Ok(new { 
+                    user = user,
+                    token = token
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anonymous";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize(Roles = "employee,manager")]
+        public string Authenticated() => String.Format("Authenticated - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Employee";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Manager";
+
+
+    }
+}
---
+++ Shop/Settings.cs
@@ -0,0 +1,8 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "5a3d1a84-2f23-4f9b-9759-6be40149455d";
+        
+    }
+}
